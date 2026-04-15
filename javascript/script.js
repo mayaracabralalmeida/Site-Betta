2793cbb89f186fe7ffcb0fe00e0542af73d87588
Luegin2 <miguel.filho@super10.com.br>
2026-04-15 20:19:34 -0300

    atualização

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#botao_mobile').on('click', function(){
+        $('#mobile_menu').toggleClass('active');
+        $('#botao_mobile').find(i).toggleClass('fa-x');
+    })
+})
